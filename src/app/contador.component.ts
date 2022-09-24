1045f5de50d925bdd2c4f28aadc40eb2111fdbfa
melissa alvarez <melissaalh24@gmail.com>
2022-09-24 11:00:52 -0500

    primera creación de un componente en angular

---
+++ src/app/contador.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  template: `<h1>{{ titulo }}</h1>
+    <h3>
+      La base es: <strong>{{ base }}</strong>
+    </h3>
+
+    <button (click)="acumulador(base)">+{{ base }}</button>
+
+    <span>{{ numero }}</span>
+
+    <button (click)="acumulador(-base)">-{{ base }}</button> `,
+})
+export class ContadorComponent {
+  titulo: string = 'Contador App';
+  numero: number = 10;
+  base: number = 5;
+
+  acumulador(valor: number) {
+    this.numero += valor;
+  }
+}
