8d421e5895483501d7b4b13e6cc0ac5ebaf524a1
Melissa <malvarez@datagas.com.co>
2022-09-28 17:50:08 -0500

    empezando a pasar información por componentes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 1000000,
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 20112,
+    },
+  ];
 
-  constructor() { }
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0,
+  };
 
-  ngOnInit(): void {
+  agregar(): void {
+    if (this.nuevo.nombre.trim().length === 0) return;
+    console.log(this.nuevo);
+
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0,
+    };
   }
-
 }
