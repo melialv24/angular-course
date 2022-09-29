f67e8f19612cb36fa86d68f904c8f267c7b5a427
Melissa <malvarez@datagas.com.co>
2022-09-29 17:25:00 -0500

    @input

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-personajes',
   templateUrl: './personajes.component.html',
 })
 export class PersonajesComponent {
-  personajes: any[] = [];
+  //aquí le digo que el valor personajes va a recibir el valor asociado a data
+  //@Input('data') personajes: any[] = [];
+  @Input() personajes: Personaje[] = [];
 }
